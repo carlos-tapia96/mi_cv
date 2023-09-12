b6e10aaa8c2a7e60d90a37a124610a0bf4c5a5cb
carlos tapia <johnncarlos7@gmail.com>
2023-09-12 17:42:34 -0400

    footer, nav, contact, visual

--- contact/forms.py
+++ contact/forms.py
@@ -6,17 +6,17 @@ class ContactForm(forms.ModelForm):
 
 	name = forms.CharField(max_length=100, required=True,
 		widget=forms.TextInput(attrs={
-			'placeholder': '*Full name..',
+			'placeholder': 'Nombre Completo..',
 			'class': 'form-control'
 			}))
 	email = forms.EmailField(max_length=254, required=True, 
 		widget=forms.TextInput(attrs={
-			'placeholder': '*Email..',
+			'placeholder': 'Email..',
 			'class': 'form-control'
 			}))
 	message = forms.CharField(max_length=1000, required=True, 
 		widget=forms.Textarea(attrs={
-			'placeholder': '*Message..',
+			'placeholder': 'Mensaje..',
 			'class': 'form-control',
 			'rows': 6,
 			}))
